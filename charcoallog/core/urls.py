ef6756f68d5f2b9c489dad322a87c99c91fe92ac
Herbert Parentes Fortes Neto <hpfn@debian.org>
2017-02-07 11:01:17 -0200

    home view

---
+++ charcoallog/core/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import url
+from charcoallog.core.views import home
+
+
+urlpatterns = [
+        url(r'^$', home, name='home'),
+]                                                                               
