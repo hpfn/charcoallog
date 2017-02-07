9a1e1d69ee2bf59ff7a5ac4e56d1695693398e34
Herbert Parentes Fortes Neto <hpfn@debian.org>
2017-02-07 10:43:23 -0200

    register method

--- charcoallog/accounts/views.py
+++ charcoallog/accounts/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.conf import settings
 
-# Create your views here.
+
+def register(request):
+    template_name = 'accounts/register.html'
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect(settings.LOGIN_URL)
+    else:
+        form = UserCreationForm()
+
+    context = {
+            'form': form
+    }
+
+    return render(request, template_name, context)
+
