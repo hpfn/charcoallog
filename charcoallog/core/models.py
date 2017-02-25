490ad85696359c500edb46bf1fa8f5139306bcd4
Herbert Parentes Fortes Neto <hpfn@debian.org>
2017-02-25 17:49:04 -0300

    money field with default. search method first try

--- charcoallog/core/models.py
+++ charcoallog/core/models.py
@@ -2,13 +2,13 @@ from django.db import models
 # from django.utils import timezone
 
 class ExtractManager(models.Manager):
-    def search(self,query):
-        return self.get_queryset().filter(models.Q(user_name__contains=query))
+    def search(self, query):
+        return self.get_queryset().filter(models.Q(user_name__contains=query).order_by('date'))
 
 class Extract(models.Model):
     user_name = models.CharField('Name', max_length=30)
-    date = models.DateField('Date', null=False, blank=False)
-    money = models.FloatField('Money', null=False, blank=False)
+    date = models.DateField('Date')
+    money = models.FloatField('Money', default=00.00, null=False, blank=False)
     description = models.CharField('Description', max_length=70)
     category = models.CharField('Category', max_length=70)
     payment = models.CharField('Payment', max_length=70)
