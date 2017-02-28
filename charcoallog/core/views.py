4b298b6fada9fcd7742888586dd31ac4c7f1401e
Herbert Parentes Fortes Neto <hpfn@debian.org>
2017-02-28 17:30:29 -0300

    one view for each action. views to frameset html files.

--- charcoallog/core/views.py
+++ charcoallog/core/views.py
@@ -1,19 +1,58 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.core.urlresolvers import reverse
+# from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.db import IntegrityError, transaction
-from charcoallog.core.models import Extract
-from charcoallog.core.forms import EditExtractForm
+from .models import Extract
+from .forms import EditExtractForm, SelectExtractForm
+
 
 # Create your views here.
 @login_required
 def home(request):
+    return render(request, "principal.html")
+
+
+@login_required
+def titulo(request):
+    return render(request, "frameset_pages/titulo.html")
+
+
+@login_required
+def row2(request):
+    return render(request, "frameset_pages/row2.html")
+
+
+@login_required
+def coluna1(request):
+    return render(request, "frameset_pages/coluna1.html")
+
+
+@login_required
+def form1(request):
+    return render(request, "frameset_pages/form1.html")
+
+
+@login_required
+def form2(request):
+    return render(request, "frameset_pages/form2.html")
+
+
+@login_required
+def rodape(request):
+    return render(request, "frameset_pages/rodape.html")
+
+
+@login_required
+def show_data(request):
     user = request.user
     builds = Extract.objects.filter(user_name=user).order_by('date')
+
     if request.method == 'POST':
         form = EditExtractForm(request.POST)
+
         if form.is_valid():
-            NewData = []
+            newdata = []
             user_name = form.cleaned_data.get('user_name')
             date = form.cleaned_data.get('date')
             money = form.cleaned_data.get('money')
@@ -21,28 +60,67 @@ def home(request):
             category = form.cleaned_data.get('category')
             payment = form.cleaned_data.get('payment')
             # if all vars
-            NewData.append(Extract(user_name=user_name, date=date,
-            money=money, description=description, category=category,
-            payment=payment))
+            newdata.append(Extract(user_name=user_name, date=date,
+                                   money=money, description=description, category=category,
+                                   payment=payment))
 
             try:
                 with transaction.atomic():
-                    Extract.objects.bulk_create(NewData)
+                    Extract.objects.bulk_create(newdata)
                     # notify user is ok ? messages()
             except IntegrityError:
                 # messages()
                 print("An error happened")
                 return redirect(reverse('Extract-settings'))
 
-            return redirect('core:home')
-    else:
-        form = EditExtractForm()
+        # return redirect('core:home')
+        builds = Extract.objects.filter(user_name=user).order_by('date')
 
-    template_name = 'home.html'
+    elif request.method == 'GET':
+        get_form = SelectExtractForm(request.GET)
+
+        if get_form.is_valid():
+            user_name = get_form.cleaned_data.get('user_name')
+            columm = get_form.cleaned_data.get('columm')
+
+            builds = Extract.objects.filter(user_name=user_name,
+                                            payment=columm).order_by('date')
+
+            if not builds:
+                builds = Extract.objects.filter(user_name=user_name,
+                                                category=columm).order_by('date')
+
+            if not builds:
+                builds = Extract.objects.filter(user_name=user_name,
+                                                description=columm).order_by('date')
+
+    template_name = 'frameset_pages/linha3.html'
     context = {
-            'builds': builds,
-            'form' : form,
+        'builds': builds,
     }
 
     return render(request, template_name, context)
 
+
+@login_required
+def show_choice_data(request):
+    get_form = SelectExtractForm()
+
+    template_name = "frameset_pages/form2.html"
+    context = {
+        'get_form': get_form,
+    }
+
+    return render(request, template_name, context)
+
+
+@login_required
+def insert_data_form(request):
+    form = EditExtractForm()
+
+    template_name = "frameset_pages/form1.html"
+    context = {
+        'form': form,
+    }
+
+    return render(request, template_name, context)
