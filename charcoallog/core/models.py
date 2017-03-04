1e0cd19a43e410b25ec5a998f582ade830b9cd93
Herbert Parentes Fortes Neto <hpfn@debian.org>
2017-03-04 10:53:11 -0300

    add *get and *post methods. came from views.

--- charcoallog/core/models.py
+++ charcoallog/core/models.py
@@ -1,12 +1,88 @@
+from django.shortcuts import redirect
 from django.db import models
+from django.db.models import Sum
+from django.db import IntegrityError, transaction
+from django.core.urlresolvers import reverse
+
+
 # from django.utils import timezone
+# import Q ?
 
 class ExtractManager(models.Manager):
-    def search(self, query):
-        return self.get_queryset().filter(models.Q(user_name__contains=query).order_by('date'))
+    def search_from_get(self, form):
+        user_name = form.cleaned_data.get('user_name')
+        columm = form.cleaned_data.get('columm')
+        from_date = form.cleaned_data.get('from_date')
+        to_date = form.cleaned_data.get('to_date')
 
-    def delete(self,query):
+        if columm.lower() == 'all':
+            builds = self.filter(user_name=user_name).filter(
+                date__gte=from_date,
+                date__lte=to_date).order_by('date')
+            total = self.filter(user_name=user_name).filter(
+                date__gte=from_date,
+                date__lte=to_date).aggregate(Sum('money'))
+        else:
+            builds = self.filter(user_name=user_name, payment=columm).filter(
+                date__gte=from_date, date__lte=to_date).order_by('date')
+            total = self.filter(user_name=user_name,
+                                payment=columm).filter(
+                date__gte=from_date,
+                date__lte=to_date).aggregate(Sum('money'))
+
+            if not builds:
+                builds = self.filter(user_name=user_name,
+                                     category=columm).filter(
+                    date__gte=from_date,
+                    date__lte=to_date).order_by('date')
+                total = self.filter(user_name=user_name,
+                                    category=columm).filter(
+                    date__gte=from_date,
+                    date__lte=to_date).aggregate(Sum('money'))
+
+            if not builds:
+                builds = self.filter(user_name=user_name,
+                                     description=columm).filter(
+                    date__gte=from_date,
+                    date__lte=to_date).order_by('date')
+                total = self.filter(user_name=user_name,
+                                    description=columm).filter(
+                    date__gte=from_date,
+                    date__lte=to_date).aggregate(Sum('money'))
+
+        return builds, total
+        # return self.get_queryset().filter(models.Q(user_name__contains=query).order_by('date'))
+
+    def insert_by_post(self, form):
+        newdata = []
+        user_name = form.cleaned_data.get('user_name')
+        date = form.cleaned_data.get('date')
+        money = form.cleaned_data.get('money')
+        description = form.cleaned_data.get('description')
+        category = form.cleaned_data.get('category')
+        payment = form.cleaned_data.get('payment')
+        remove = form.cleaned_data.get('remove')
+        # if all vars, but no remove.
+        newdata.append(Extract(user_name=user_name, date=date, money=money,
+                               description=description, category=category, payment=payment))
+
+        try:
+            with transaction.atomic():
+                if remove:
+                    self.filter(user_name=user_name, date=date,
+                                money=money, description=description,
+                                category=category, payment=payment).delete()
+                else:
+                    self.bulk_create(newdata)
+                    # notify user is ok ? messages()
+        except IntegrityError:
+            # messages()
+            print("An error happened")
+            return redirect(reverse('Extract-settings'))
+
+    def delete(self, query):
         return self.filter(query).delete()
+
 
 class Extract(models.Model):
     user_name = models.CharField('Name', max_length=30)
