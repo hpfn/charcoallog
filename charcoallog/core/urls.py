8453f421b7b5714461993f49e5f9d384e296e757
Herbert Parentes Fortes Neto <hpfn@debian.org>
2017-03-04 10:43:22 -0300

    remove coluna1.html url

--- charcoallog/core/urls.py
+++ charcoallog/core/urls.py
@@ -1,15 +1,13 @@
 from django.conf.urls import url
 from .views import home, titulo, rodape
-from .views import row2, coluna1
+from .views import row2
 from .views import insert_data_form
 from .views import show_data, show_choice_data, show_total
-
 
 urlpatterns = [
     url(r'^$', home, name='home'),
     url(r'^frameset_pages/titulo.html$', titulo, name='titulo'),
     url(r'^frameset_pages/row2.html$', row2, name='row2'),
-    url(r'^frameset_pages/coluna1.html$', coluna1, name='coluna1'),
     url(r'^frameset_pages/form1.html$', insert_data_form, name='form1'),
     url(r'^frameset_pages/form2.html$', show_choice_data, name='form2'),
     url(r'^frameset_pages/linha1.html$', show_total, name='show_total'),
