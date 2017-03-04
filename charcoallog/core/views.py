38d2aac767bbddb456310da5e4a9c6100939adac
Herbert Parentes Fortes Neto <hpfn@debian.org>
2017-03-04 10:41:42 -0300

    less views. request 'post' and 'get' goes to models.

--- charcoallog/core/views.py
+++ charcoallog/core/views.py
@@ -25,21 +25,6 @@ def row2(request):
 
 
 @login_required
-def coluna1(request):
-    return render(request, "frameset_pages/coluna1.html")
-
-
-@login_required
-def form1(request):
-    return render(request, "frameset_pages/form1.html")
-
-
-@login_required
-def form2(request):
-    return render(request, "frameset_pages/form2.html")
-
-
-@login_required
 def rodape(request):
     return render(request, "frameset_pages/rodape.html")
 
@@ -54,34 +39,8 @@ def show_data(request):
         form = EditExtractForm(request.POST)
 
         if form.is_valid():
-            newdata = []
-            user_name = form.cleaned_data.get('user_name')
-            date = form.cleaned_data.get('date')
-            money = form.cleaned_data.get('money')
-            description = form.cleaned_data.get('description')
-            category = form.cleaned_data.get('category')
-            payment = form.cleaned_data.get('payment')
-            remove = form.cleaned_data.get('remove')
-            # if all vars, but no remove.
-            newdata.append(Extract(user_name=user_name, date=date,
-                                   money=money, description=description, category=category,
-                                   payment=payment))
+            Extract.objects.insert_by_post(form)
 
-            try:
-                with transaction.atomic():
-                    if remove:
-                        Extract.objects.filter(user_name=user_name, date=date,
-                                               money=money, description=description,
-                                               category=category, payment=payment).delete()
-                    else:
-                        Extract.objects.bulk_create(newdata)
-                        # notify user is ok ? messages()
-            except IntegrityError:
-                # messages()
-                print("An error happened")
-                return redirect(reverse('Extract-settings'))
-
-        # return redirect('core:home')
         builds = Extract.objects.filter(user_name=user).order_by('date')
         total = Extract.objects.filter(user_name=user).aggregate(Sum('money'))
 
@@ -89,28 +48,7 @@ def show_data(request):
         get_form = SelectExtractForm(request.GET)
 
         if get_form.is_valid():
-            user_name = get_form.cleaned_data.get('user_name')
-            columm = get_form.cleaned_data.get('columm')
-
-            if columm.lower() == 'all':
-                builds = Extract.objects.filter(user_name=user_name).order_by('date')
-                total = Extract.objects.filter(user_name=user_name).aggregate(Sum('money'))
-            else:
-                builds = Extract.objects.filter(user_name=user_name,
-                                                payment=columm).order_by('date')
-                total = Extract.objects.filter(user_name=user_name,
-                                               payment=columm).aggregate(Sum('money'))
-
-                if not builds:
-                    builds = Extract.objects.filter(user_name=user_name,
-                                                    category=columm).order_by('date')
-                    total = Extract.objects.filter(user_name=user_name,
-                                                   category=columm).aggregate(Sum('money'))
-                if not builds:
-                    builds = Extract.objects.filter(user_name=user_name,
-                                                    description=columm).order_by('date')
-                    total = Extract.objects.filter(user_name=user_name,
-                                                   description=columm).aggregate(Sum('money'))
+            builds, total = Extract.objects.search_from_get(get_form)
 
     template_name = 'frameset_pages/linha3.html'
     context = {
@@ -144,6 +82,7 @@ def insert_data_form(request):
 
     return render(request, template_name, context)
 
+
 def show_total(request):
     user = request.user
 
@@ -156,7 +95,6 @@ def show_total(request):
     saldo = 0.0
     for resto in total_account:
         saldo += float(resto['money__sum'])
-
 
     template_name = 'frameset_pages/linha1.html'
     context = {
