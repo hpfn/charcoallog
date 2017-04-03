7d749b5aed3c36b62daf9a2a090e368edb6e5f13
Herbert Parentes Fortes Neto <hpfn@debian.org>
2017-04-03 10:33:43 -0300

    running.

--- charcoallog/settings.py
+++ charcoallog/settings.py
@@ -121,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 MEDIA_ROOT = os.path.join(BASE_DIR, 'charcoallog', 'media')                      
 MEDIA_URL = '/media/' 
 
