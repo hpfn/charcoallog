4725017ad72dc0385d31f058ffd68028a2f40983
Herbert Parentes Fortes Neto <hpfn@debian.org>
2017-07-15 15:31:03 -0300

    decouple in use

--- charcoallog/settings.py
+++ charcoallog/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+from decouple import config, Csv
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,12 +21,15 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'kxb7_ulllfxr-qzm4^imvwb2i$pz8zvy)vyr&fjy=ec126$wys'
+#SECRET_KEY = 'kxb7_ulllfxr-qzm4^imvwb2i$pz8zvy)vyr&fjy=ec126$wys'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+#ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default=[], cast=Csv())
 
 
 # Application definition
