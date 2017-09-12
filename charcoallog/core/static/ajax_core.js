7d0cd9ec088f9ea05b18bfd5fd9ed0b6ec181c95
Herbert Parentes Fortes Neto <hpfn@debian.org>
2017-09-12 11:30:43 -0300

    remove form by id test.

--- charcoallog/core/static/ajax_core.js
+++ charcoallog/core/static/ajax_core.js
@@ -1,8 +1,27 @@
 $(function() {
+    var rm;
+    var form_rm;
     $("#box_line3 form").on('submit', function(e) {
         e.preventDefault();
-        var data_v = $(this).serialize();
+        var data_v = $(this).serializeArray();
+
         console.log(data_v);
+
+        $.each(data_v, function(i, field) {
+            if ( field.name == "id") {
+                form_rm = '#' + field.value;
+            }
+            if ( field.value == "remove") {
+                console.log('SIMMMM');
+                $(form_rm).remove();
+            }
+        });
+
+        console.log($(data_v));
         $('input').attr('readonly', 'true');
+
+        if ( $(data_v['update_rm']).val() == 'remove' ) {
+            console.log('SIMMMM');
+        }
     });
 });
