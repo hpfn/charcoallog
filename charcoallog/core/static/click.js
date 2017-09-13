fd579230745711fe946ed16ec491b2d29aedc393
Herbert Parentes Fortes Neto <hpfn@debian.org>
2017-09-13 14:04:35 -0300

    no console log, comments cleaned, focusout red

--- charcoallog/core/static/click.js
+++ charcoallog/core/static/click.js
@@ -1,17 +1,14 @@
 $(function() {
     $("#box_line3 input").bind('click', function() {
         if ( $(this).val() == 'update') {
-            console.log( $(this).val() );
-            console.log("clicked");
-            console.log( $(this).parents("table").find('input').removeAttr('readonly') );
-            //console.log( $(this).parent().parent().parent().find('#category').val() );
-            //console.log( $(this).parent().parent().parent().parent().find('input').removeAttr('readonly') );
-            //console.log( $(this).parent().parent().parent().parent().parent().find('#money').val() );
-            //console.log( $(this).parent().parent().parent().parent().parent().parent().find('#date').val() );
-            //console.log( $(this).parent().parent().parent().parent().parent().parent().parent().find('#id').val() );
-            //console.log( $(this).parent().parent().parent().parent().parent().parent().parent().parent().find('#user_name').val() );
-
-            //$('#payment').removeAttr('readonly');
+            //console.log( $(this).val() );
+            //console.log("clicked");
+            $(this).parents("table").find('input').removeAttr('readonly');
         }
-    });//$("input").attrRemove("readonly");
+    });
+    $("#box_line3 input").focusout(function() {
+        if ( $(this).val() < 0 ) {
+            $(this).css('color', 'red');
+        }
+    });
 });
