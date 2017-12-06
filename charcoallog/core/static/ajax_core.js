c1bbb2cc3880278afb3a3f17b36648247bd10d18
Herbert Parentes Fortes Neto <hpfn@debian.org>
2017-12-06 15:21:59 -0200

    update total of an account

--- charcoallog/core/static/ajax_core.js
+++ charcoallog/core/static/ajax_core.js
@@ -1,4 +1,28 @@
 $(function() {
+    var old_money;
+    $("#box_line3 input").bind('click', function() {
+        if ( $(this).val() == 'update') {
+            //console.log( $(this).val() );
+            //console.log("clicked");
+            $(this).parents("table").find('input').removeAttr('readonly');
+        }
+    });
+    $("#box_line3 input").focusin(function() {
+        if ( Number.isFinite(Number($(this).val())) ) {
+            old_money = $(this).val();
+            //console.log('focus in');
+            //console.log(old_money);
+        }
+    });
+    $("#box_line3 input").focusout(function() {
+        if ( $(this).val() < 0 ) {
+            $(this).css('color', 'red');
+        } else {
+            $(this).css('color', 'black');
+        }
+    });
+
+
     $("#box_line3 form").on('submit', function(e) {
         e.preventDefault();
         var data_v = $(this).serializeArray();
@@ -7,20 +31,28 @@ $(function() {
             url: '/',
             data: data_v,
             success: function(content) {
+                console.log("old money before submit")
+                console.log(old_money);
                 if ( data_v[8].value == 'remove' ) {
                     $('#'+data_v[2].value).remove();
                 }
                 if ( data_v[8].value == 'update' ) {
-                    //$('#'+data_v[2].value + ' input:radio[name=update_rm]').attr('checked', false);
                     $('#'+data_v[2].value + ' input:radio[name=update_rm]')[1].checked = true;
                     $('#'+data_v[2].value + " input").attr('readonly', 'true');
-                    var account_1 = $('#'+data_v[7].value).text().trim();
-                    console.log($('#'+data_v[7].value).text().trim());
-                    // value on line1.html + sent value + old form value # fix it
-                    account_1 = Number(account_1) + Number(data_v[4].value);
+                    // update total of each account. line1.html
+                    var old_total_account = $('#'+data_v[7].value).text().trim();
+                    var less_old_money = Number(old_total_account) - Number(old_money);
+                    var account_1 = Number(less_old_money) + Number(data_v[4].value);
                     $('#'+data_v[7].value).text(account_1);
-                    console.log(account_1);
-                    console.log(data_v[7].value);
+                    //console.log('old total account');
+                    //console.log(old_total_account);
+                    //console.log('valor antigo');
+                    //console.log(old_money);
+                    //console.log('total antigo - valor antigo');
+                    //console.log(less_old_money);
+                    //console.log('resultado da op');
+                    //console.log(account_1);
+                    //console.log(data_v[7].value);
                 }
             },
             error: function(content) {
