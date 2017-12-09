d5fdb6559c5c16bed25889071e23a30587aaa763
Herbert Parentes Fortes Neto <hpfn@debian.org>
2017-12-09 11:29:38 -0200

    show_date() using new queryset statement

--- charcoallog/core/views.py
+++ charcoallog/core/views.py
@@ -2,8 +2,9 @@ from datetime import date
 from collections import OrderedDict
 
 from django.contrib.auth.decorators import login_required
-from django.db.models import Sum
+# from django.db.models import Sum
 from django.shortcuts import render
+from .manager import insert_by_post, search_from_get
 
 from .forms import EditExtractForm, SelectExtractForm
 from .models import Extract
@@ -41,37 +42,38 @@ def show_data(request):
 
         if form.is_valid():
             form.cleaned_data['user_name'] = user
-            Extract.objects.insert_by_post(form)
+            # Extract.objects.insert_by_post(form)
+            insert_by_post(form)
     elif request.method == 'GET':
         get_form = SelectExtractForm(request.GET)
 
         if get_form.is_valid():
-            return Extract.objects.search_from_get(request, get_form)
+            # return Extract.objects.search_from_get(request, get_form)
+            return search_from_get(request, get_form)
 
-    bills = Extract.objects.filter(user_name=user).filter(date__gte=d)
-    #total = Extract.objects.filter(user_name=user).filter(date__gte=d).aggregate(Sum('money'))
-    total = bills.aggregate(Sum('money'))
+    bills = Extract.objects.user_logged(user).filter(date__gte=d)
+    # total = Extract.objects.filter(user_name=user).filter(date__gte=d).aggregate(Sum('money'))
+    # total = bills.aggregate(Sum('money'))
 
-    return bills, total
+    return bills, bills.total()
 
 
 @login_required
 def show_total(request):
     user = request.user
 
-    payment_iterator = set(Extract.objects.filter(user_name=user).values_list(
+    payment_iterator = set(Extract.objects.user_logged(user).values_list(
         'payment'))
     payment_list = [i[0] for i in payment_iterator]
 
-    #total_account = [Extract.objects.filter(
+    # total_account = [Extract.objects.filter(
     #    user_name=user, payment=conta).aggregate(Sum('money'))
     #                 for conta in payment_list]
 
     total_account = dict()
     for conta in payment_list:
-        total_account[conta] = Extract.objects.filter(
-            user_name=user, payment=conta).aggregate(Sum('money'))
-
+        total_account[conta] = Extract.objects.user_logged(user).filter(
+            payment=conta).total()
 
     saldo = 0
     for resto in total_account.values():
