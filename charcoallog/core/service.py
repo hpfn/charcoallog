1fee710f4ca5651b8a93a70efa811e994f1ad31d
Herbert Parentes Fortes Neto <hpfn@debian.org>
2017-12-10 14:49:37 -0200

    one view. rename manager -> service (class ShowData)

---
+++ charcoallog/core/service.py
@@ -0,0 +1,79 @@
+from collections import OrderedDict
+from datetime import date
+
+from django.contrib import messages
+from .forms import EditExtractForm, SelectExtractForm
+from .models import Extract
+
+
+class ShowData:
+    def __init__(self, request):
+        self.request = request
+        self.month_01 = date.today().strftime('%Y-%m-01')
+        self.query_root = Extract.objects.user_logged(self.request.user)
+        self.query_default = self.query_root.filter(date__gte=self.month_01)
+
+    def method_post(self):
+        form = EditExtractForm(self.request.POST)
+
+        if form.is_valid():
+            form.cleaned_data['user_name'] = self.request.user
+            self.insert_by_post(form)
+
+        return self.query_default
+
+    def method_get(self):
+        bills = 0
+        get_form = SelectExtractForm(self.request.GET)
+
+        if get_form.is_valid():
+            bills = self.search_from_get(get_form)
+
+        return bills or self.query_default
+
+    def search_from_get(self, form):
+        column = form.cleaned_data.get('column')
+        from_date = form.cleaned_data.get('from_date')
+        to_date = form.cleaned_data.get('to_date')
+
+        if column.lower() == 'all':
+            bills = self.query_root.date_range(from_date, to_date)
+        else:
+            bills = self.query_root.date_range(from_date, to_date).which_field(column)
+
+        if bills.exists():
+            return bills
+
+        messages.error(self.request,
+                       "' %s ' is an Invalid search or wrong date!" % column)
+
+    def insert_by_post(self, form):
+        what_to_do = form.cleaned_data.get('update_rm')
+        del form.cleaned_data['update_rm']
+        id_for_update = form.cleaned_data.get('pk')
+        del form.cleaned_data['pk']
+
+        if what_to_do == 'remove':
+            Extract.objects.filter(**form.cleaned_data).delete()
+        elif what_to_do == 'update':
+            obj = Extract.objects.get(id=id_for_update, user_name=self.request.user)
+            obj.date = form.cleaned_data['date']
+            obj.money = form.cleaned_data['money']
+            obj.description = form.cleaned_data['description']
+            obj.category = form.cleaned_data['category']
+            obj.payment = form.cleaned_data['payment']
+            obj.save(update_fields=['date', 'money', 'description', 'category', 'payment'])
+        else:
+            form.save()
+
+    def show_total(self):
+        payment_iterator = set(self.query_root.values_list('payment'))
+
+        total_account = {
+            conta[0]: self.query_root.filter(payment=conta[0]).total()
+            for conta in payment_iterator
+        }
+
+        saldo = sum([resto['money__sum']for resto in total_account.values()])
+
+        return OrderedDict(sorted(total_account.items())), saldo
--- charcoallog/core/views.py
+++ charcoallog/core/views.py
@@ -1,22 +1,22 @@
-from collections import OrderedDict
-from datetime import date
-
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
-
 from .forms import EditExtractForm, SelectExtractForm
-from .manager import insert_by_post, search_from_get
-from .models import Extract
+from .service import ShowData  # insert_by_post, search_from_get
 
 
 @login_required
 def home(request):
-    bills = show_data(request)
-    total_account, saldo = show_total(request)
+    bills = ShowData(request)
+    if request.method == 'POST':
+        bills_query = bills.method_post()
+    else:
+        bills_query = bills.method_get()
+
+    total_account, saldo = bills.show_total()
 
     context = {
-        'bills': bills,
-        'total': bills.total(),
+        'bills': bills_query,
+        'total': bills_query.total(),
         'form': EditExtractForm(),
         'get_form': SelectExtractForm(),
         'total_account': total_account,
@@ -25,42 +25,42 @@ def home(request):
     return render(request, "home.html", context)
 
 
-@login_required
-def show_data(request):
-    d = date.today().strftime('%Y-%m-01')
-    bills = 0
+# @login_required
+# def show_data(request):
+#     d = date.today().strftime('%Y-%m-01')
+#     bills = 0
+#
+#     if request.method == 'POST':
+#         form = EditExtractForm(request.POST)
+#
+#         if form.is_valid():
+#             form.cleaned_data['user_name'] = request.user
+#             insert_by_post(form)
+#
+#     elif request.method == 'GET':
+#         get_form = SelectExtractForm(request.GET)
+#
+#         if get_form.is_valid():
+#             bills = search_from_get(request, get_form)
+#
+#     return bills or Extract.objects.user_logged(request.user).filter(date__gte=d)
+#
 
-    if request.method == 'POST':
-        form = EditExtractForm(request.POST)
-
-        if form.is_valid():
-            form.cleaned_data['user_name'] = request.user
-            insert_by_post(form)
-
-    elif request.method == 'GET':
-        get_form = SelectExtractForm(request.GET)
-
-        if get_form.is_valid():
-            bills = search_from_get(request, get_form)
-
-    return bills or Extract.objects.user_logged(request.user).filter(date__gte=d)
-
-
-@login_required
-def show_total(request):
-    payment_iterator = set(Extract.objects.user_logged(
-        request.user).values_list('payment'))
-
-    total_account = {
-        conta[0]: Extract.objects.user_logged(request.user).filter(
-            payment=conta[0]).total() for conta in payment_iterator
-    }
-    # total_account = dict()
-    # for set_value in payment_iterator:
-    #    conta = set_value[0]
-    #    total_account[conta] = Extract.objects.user_logged(request.user).filter(
-    #        payment=conta).total()
-
-    saldo = sum([resto['money__sum']for resto in total_account.values()])
-
-    return OrderedDict(sorted(total_account.items())), saldo
+# @login_required
+# def show_total(request):
+#     payment_iterator = set(Extract.objects.user_logged(
+#         request.user).values_list('payment'))
+#
+#     total_account = {
+#         conta[0]: Extract.objects.user_logged(request.user).filter(
+#             payment=conta[0]).total() for conta in payment_iterator
+#     }
+#     # total_account = dict()
+#     # for set_value in payment_iterator:
+#     #    conta = set_value[0]
+#     #    total_account[conta] = Extract.objects.user_logged(request.user).filter(
+#     #        payment=conta).total()
+#
+#     saldo = sum([resto['money__sum']for resto in total_account.values()])
+#
+#     return OrderedDict(sorted(total_account.items())), saldo
