17b298c1cfb7e0af7220638d24cc627ccb6f41d8
Herbert Parentes Fortes Neto <hpfn@debian.org>
2017-12-11 12:48:58 -0200

    context with one item. all .html files refer it

--- charcoallog/core/views.py
+++ charcoallog/core/views.py
@@ -6,15 +6,7 @@ from .service import ShowData  # insert_by_post, search_from_get
 
 @login_required
 def home(request):
-    bills = ShowData(request)
-    #total_account, saldo = bills.show_total()
-
     context = {
-        'bills': bills,
-        'total': bills,
-        'form': bills,
-        'get_form': bills,
-        'total_account': bills,
-        'saldo': bills,
+        'show_data': ShowData(request),
     }
     return render(request, "home.html", context)
