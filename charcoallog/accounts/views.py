2eac282e1a59198a5586e8e36551fd4b9fc976b3
Herbert Parentes Fortes Neto <hpfn@debian.org>
2017-12-14 11:28:05 -0200

    form with email field.

--- charcoallog/accounts/views.py
+++ charcoallog/accounts/views.py
@@ -1,23 +1,26 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+# from django.contrib.auth.forms import UserCreationForm
 from django.conf import settings
+
+from .forms import RegisterForm
 
 
 def register(request):
     template_name = 'accounts/register.html'
 
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        # form = UserCreationForm(request.POST)
+        form = RegisterForm(request.POST)
 
         if form.is_valid():
             form.save()
             return redirect(settings.LOGIN_URL)
     else:
-        form = UserCreationForm()
+        # form = UserCreationForm()
+        form = RegisterForm()
 
     context = {
-            'form': form
+        'form': form
     }
 
     return render(request, template_name, context)
-
