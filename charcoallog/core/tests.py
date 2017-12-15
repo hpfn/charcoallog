9b6aa19b8b0c52afab59991ca6b9d4229f423778
Herbert Parentes Fortes Neto <hpfn@debian.org>
2017-12-15 17:56:41 -0200

    get 'home.html' template test

--- charcoallog/core/tests.py
+++ charcoallog/core/tests.py
@@ -1,3 +1,41 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
 
-# Create your tests here.
+
+# class HomeFailTest(TestCase):
+#     def setUp(self):
+#         self.response = self.client.get('/')
+#
+#     def test_fail_response(self):
+#         self.assertEqual(302, self.response.status_code)
+#
+#
+# class RedirectToLogin(TestCase):
+#     def setUp(self):
+#         self.c = Client()
+#         self.response = self.c.get('/', follow=True)
+#
+#     # def test_loginfail(self):
+#     #    self.assertFalse(self.c.login(username='teste', password='1qa2ws3ed'))
+#
+#     def test_redirect(self):
+#         self.assertTemplateUsed(self.response, 'accounts/login.html')
+
+
+class HomeOKTest(TestCase):
+    def setUp(self):
+        user = User.objects.create(username='teste')
+        user.set_password('1qa2ws3ed')
+        user.save()
+        self.c = Client()
+        self.login_in = self.c.login(username='teste', password='1qa2ws3ed')
+        self.response = self.c.get('/')
+
+    def test_login(self):
+        self.assertTrue(self.login_in)
+
+    def test_get_root(self):
+        self.assertEqual(200, self.response.status_code)
+
+    def test_template(self):
+        self.assertTemplateUsed(self.response, 'home.html')
