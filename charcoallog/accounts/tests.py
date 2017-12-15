d0c704fdf43e5b52da696ebb47936840ca16027c
Herbert Parentes Fortes Neto <hpfn@debian.org>
2017-12-15 17:57:47 -0200

    fail to get '/' and redirect to 'login.html' tests.

--- charcoallog/accounts/tests.py
+++ charcoallog/accounts/tests.py
@@ -1,3 +1,21 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
-# Create your tests here.
+
+class HomeFailTest(TestCase):
+    def setUp(self):
+        self.response = self.client.get('/')
+
+    def test_fail_response(self):
+        self.assertEqual(302, self.response.status_code)
+
+
+class RedirectToLogin(TestCase):
+    def setUp(self):
+        self.c = Client()
+        self.response = self.c.get('/', follow=True)
+
+    # def test_loginfail(self):
+    #    self.assertFalse(self.c.login(username='teste', password='1qa2ws3ed'))
+
+    def test_redirect(self):
+        self.assertTemplateUsed(self.response, 'accounts/login.html')
