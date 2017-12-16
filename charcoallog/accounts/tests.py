46cdb2dc2fcd413badc80999e9261079741d113f
Herbert Parentes Fortes Neto <hpfn@debian.org>
2017-12-16 13:28:00 -0200

    register test

--- charcoallog/accounts/tests.py
+++ charcoallog/accounts/tests.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.forms import AuthenticationForm
 from django.test import TestCase, Client
 
+from .forms import RegisterForm
+
 
 class HomeFailTest(TestCase):
     def setUp(self):
@@ -10,7 +12,7 @@ class HomeFailTest(TestCase):
         self.assertEqual(302, self.response.status_code)
 
 
-class RedirectToLogin(TestCase):
+class RedirectToLoginTest(TestCase):
     """ Using Django login, but testing anyway """
     def setUp(self):
         self.c = Client()
@@ -49,3 +51,37 @@ class RedirectToLogin(TestCase):
         """ Form must have two fields """
         form = self.response.context['form']
         self.assertSequenceEqual(['username', 'password'], list(form.fields))
+
+
+class RegisterPageTest(TestCase):
+    def setUp(self):
+        self.response = self.client.get('/conta/cadastre-se/')
+
+    def test_status_code(self):
+        self.assertEqual(200, self.response.status_code)
+
+    def test_template(self):
+        self.assertTemplateUsed(self.response, 'accounts/register.html')
+
+    def test_html_register_form(self):
+        """" Html must contain input tags """
+        self.assertContains(self.response, '<form')
+        self.assertContains(self.response, '<input', 5)
+        self.assertContains(self.response, 'type="text"', 1)
+        self.assertContains(self.response, 'type="password"', 2)
+        self.assertContains(self.response, 'type="email"', 1)
+        self.assertContains(self.response, '<button', 1)
+        self.assertContains(self.response, 'type="submit"', 1)
+
+    def test_csrf(self):
+        self.assertContains(self.response, 'csrfmiddlewaretoken')
+
+    def test_has_form(self):
+        """ Context must have register form """
+        form = self.response.context['form']
+        self.assertIsInstance(form, RegisterForm)
+
+    def test_form_has_fields(self):
+        """ Form must have two fields """
+        form = self.response.context['form']
+        self.assertSequenceEqual(['username', 'password1', 'password2', 'email'], list(form.fields))
