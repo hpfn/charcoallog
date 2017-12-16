988f36c521ea0b11cdee47336b93028df0669b1d
Herbert Parentes Fortes Neto <hpfn@debian.org>
2017-12-16 13:44:01 -0200

    only home page tests

--- charcoallog/core/tests/tests.py
+++ charcoallog/core/tests/tests.py
@@ -2,24 +2,12 @@ from django.test import TestCase, Client
 from django.contrib.auth.models import User
 
 
-# class HomeFailTest(TestCase):
-#     def setUp(self):
-#         self.response = self.client.get('/')
-#
-#     def test_fail_response(self):
-#         self.assertEqual(302, self.response.status_code)
-#
-#
-# class RedirectToLogin(TestCase):
-#     def setUp(self):
-#         self.c = Client()
-#         self.response = self.c.get('/', follow=True)
-#
-#     # def test_loginfail(self):
-#     #    self.assertFalse(self.c.login(username='teste', password='1qa2ws3ed'))
-#
-#     def test_redirect(self):
-#         self.assertTemplateUsed(self.response, 'accounts/login.html')
+class HomeFailTest(TestCase):
+    def setUp(self):
+        self.response = self.client.get('/')
+
+    def test_fail_response(self):
+        self.assertEqual(302, self.response.status_code)
 
 
 class HomeOKTest(TestCase):
