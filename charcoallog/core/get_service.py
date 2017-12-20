7b83c176f497e651087ebf3984bffd42f742f282
Herbert Parentes Fortes Neto <hpfn@debian.org>
2017-12-20 10:42:28 -0200

    no request thread. no 'message.error'

--- charcoallog/core/get_service.py
+++ charcoallog/core/get_service.py
@@ -5,18 +5,19 @@ from .forms import SelectExtractForm
 
 
 class MethodGet:
-    def __init__(self, request_qet, query_user):
-        self.request = request_qet
+    def __init__(self, request_method, request_qet, query_user):
+        self.request_get = request_qet
         self.month_01 = date.today().strftime('%Y-%m-01')
         self.query_user = query_user
         self.query_default = self.query_user.filter(date__gte=self.month_01)
         self.query_default_total = self.query_default.total()
         self.selectextractform = SelectExtractForm
 
-        self.method_get()
+        if request_method == "GET":
+            self.method_get()
 
     def method_get(self):
-        get_form = self.selectextractform(self.request)
+        get_form = self.selectextractform(self.request_get)
 
         if get_form.is_valid():
             self.search_from_get(get_form)
@@ -35,7 +36,8 @@ class MethodGet:
             print('bills exists')
             self.query_default = bills
         else:
-            messages.error(
-                self.request,
-                "' %s ' is an Invalid search or wrong date!" % column
-            )
+            self.query_default = None
+            # messages.error(
+            #    self.request,
+            #    "' %s ' is an Invalid search or wrong date!" % column
+            #)
