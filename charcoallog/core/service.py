1a0b5860dcae978a985dc5d5c393eb41d442a8ca
Herbert Parentes Fortes Neto <hpfn@debian.org>
2017-12-20 09:16:21 -0200

    MethodPost and MethodGet services only receive request.method, request.POST|GET and request.user

--- charcoallog/core/service.py
+++ charcoallog/core/service.py
@@ -9,8 +9,8 @@ class ShowData:
         self.request = request
         self.query_user = Extract.objects.user_logged(self.request.user)
         # self.account_values = None
-        self.form1 = MethodPost(self.request, self.query_user)
-        self.form2 = MethodGet(self.request, self.query_user)
+        self.form1 = MethodPost(self.request.method, request.POST, request.user, self.query_user)
+        self.form2 = MethodGet(self.request.GET, self.query_user)
         self.line1 = Line1(self.query_user)
         self.account_names = self.line1.account_names()
         self.whats_left = self.line1.whats_left()
