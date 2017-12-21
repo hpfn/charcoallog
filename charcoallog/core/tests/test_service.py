d2c47966a4ebc8c9702408e26ce6361dce304511
Herbert Parentes Fortes Neto <hpfn@debian.org>
2017-12-21 16:37:47 -0200

    test all attr in service

--- charcoallog/core/tests/test_service.py
+++ charcoallog/core/tests/test_service.py
@@ -1,7 +1,13 @@
+from collections import OrderedDict
 from decimal import Decimal
+
+from django.db.models import QuerySet
 from django.test import TestCase
 
+from charcoallog.core.get_service import MethodGet
+from charcoallog.core.line1_service import Line1
 from charcoallog.core.models import Extract
+from charcoallog.core.post_service import MethodPost
 from charcoallog.core.service import ShowData
 
 
@@ -29,6 +35,24 @@ class ServiceLayerTest(TestCase):
         Extract.objects.create(**others_data)
         search_data = dict(column='all', from_date='2017-12-01', to_date='2017-12-31')
         self.response = ShowData('GET', search_data, dict(), user_name)
+
+    def test_query_user_instance(self):
+        self.assertIsInstance(self.response.query_user, QuerySet)
+
+    def test_form1_instance(self):
+        self.assertIsInstance(self.response.form1, MethodPost)
+
+    def test_form2_instance(self):
+        self.assertIsInstance(self.response.form2, MethodGet)
+
+    def test_line1_instance(self):
+        self.assertIsInstance(self.response.line1, Line1)
+
+    def test_account_names_attr(self):
+        self.assertEqual(
+            self.response.account_names,
+            OrderedDict([('principal', {'money__sum': Decimal('10.00')})])
+        )
 
     def test_get_total(self):
         """ whats_left attribute must be 10 for user teste """
