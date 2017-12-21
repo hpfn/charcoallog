7ca06153aaab55269f4028448bd64e3846b049ce
Herbert Parentes Fortes Neto <hpfn@debian.org>
2017-12-21 10:41:56 -0200

    formatting, optimize imports, get_form in __init__

--- charcoallog/core/get_service.py
+++ charcoallog/core/get_service.py
@@ -1,26 +1,31 @@
 from datetime import date
 
-from django.contrib import messages
-from .forms import SelectExtractForm
+from charcoallog.core.forms import SelectExtractForm
 
 
 class MethodGet:
-    def __init__(self, request_method, request_qet, query_user):
-        self.request_get = request_qet
+    def __init__(self, request_method, request_get, query_user):
+        """
+        :param request_method: should be 'GET' or 'POST'
+        :param request_get: request.GET from view home
+        :param query_user: Extract objects from ..models.py
+        """
+        self.request_get = request_get
         self.month_01 = date.today().strftime('%Y-%m-01')
         self.query_user = query_user
         self.query_default = self.query_user.filter(date__gte=self.month_01)
         self.query_default_total = self.query_default.total()
         self.selectextractform = SelectExtractForm
+        self.get_form = None
 
         if request_method == "GET":
             self.method_get()
 
     def method_get(self):
-        get_form = self.selectextractform(self.request_get)
+        self.get_form = self.selectextractform(self.request_get)
 
-        if get_form.is_valid():
-            self.search_from_get(get_form)
+        if self.get_form.is_valid():
+            self.search_from_get(self.get_form)
 
     def search_from_get(self, form):
         column = form.cleaned_data.get('column')
@@ -33,11 +38,11 @@ class MethodGet:
             bills = self.query_user.date_range(from_date, to_date).which_field(column)
 
         if bills.exists():
-            print('bills exists')
+            # print('bills exists')
             self.query_default = bills
         else:
             self.query_default = None
             # messages.error(
             #    self.request,
             #    "' %s ' is an Invalid search or wrong date!" % column
-            #)
+            # )
