c1655181be108e7a000a11ce64c6e548629ab37f
Herbert Parentes Fortes Neto <hpfn@debian.org>
2017-12-22 16:16:43 -0200

    minor fixes. get by 'id' only.

--- charcoallog/core/post_service.py
+++ charcoallog/core/post_service.py
@@ -3,20 +3,29 @@ from .forms import EditExtractForm
 
 class MethodPost:
     def __init__(self, request_method, request_post, request_user, query_user):
-        self.request_method = request_method
+        """
+        :param request_method: POST or GET
+        :param request_post: dict()
+        :param request_user: request.user
+        :param query_user: Extract models instance
+        """
+        # self.request_method = request_method
         self.request_post = request_post
         self.request_user = request_user
         self.query_user = query_user
         self.editextractform = EditExtractForm
+        self.form = None
 
         if request_method == 'POST':
             self.method_post()
 
     def method_post(self):
-        form = self.editextractform(self.request_post)
+        self.form = self.editextractform(self.request_post)
 
-        if form.is_valid():
-            self.insert_by_post(form)
+        if self.form.is_valid():
+            self.insert_by_post(self.form)
+        else:
+            print('INVALID')
 
     def insert_by_post(self, form):
         what_to_do = form.cleaned_data.get('update_rm')
@@ -31,7 +40,7 @@ class MethodPost:
         elif what_to_do == 'remove':
             self.query_user.filter(**form.cleaned_data).delete()
         elif what_to_do == 'update':
-            obj = self.query_user.get(id=id_for_update, user_name=self.request_user)
+            obj = self.query_user.get(id=id_for_update)  # , user_name=self.request_user)
             obj.date = form.cleaned_data['date']
             obj.money = form.cleaned_data['money']
             obj.description = form.cleaned_data['description']
