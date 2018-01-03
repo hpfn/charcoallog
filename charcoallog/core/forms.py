9cbc24e8a8d26ce0ddb11e7481cc93489ff36a57
Herbert Parentes Fortes Neto <hpfn@debian.org>
2018-01-03 14:32:12 -0200

    set date range year-5, year+10

--- charcoallog/core/forms.py
+++ charcoallog/core/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from .models import Extract
-
+from datetime import datetime
 
 # more recent
 # https://codedump.io/share/3seZkm5xb6mu/1/using-django-timedate-widgets-in-custom-form
@@ -45,6 +45,9 @@ class SelectExtractForm(forms.Form):
     """ Specific Columm """
     # user_name = forms.CharField(max_length=30, widget=forms.HiddenInput(),
     #                            required=True)
+    d = datetime.now()
+    year = int(d.strftime("%Y"))
+    years = [x for x in range(year-5, year+10)]
     column = forms.CharField(max_length=70, required=True)
-    from_date = forms.DateField(widget=forms.SelectDateWidget, required=True)
+    from_date = forms.DateField(widget=forms.SelectDateWidget(years=years), required=True)
     to_date = forms.DateField(widget=forms.SelectDateWidget, required=True)
