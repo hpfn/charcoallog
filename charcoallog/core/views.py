854373cf5215178c1812a6415655648226092f11
Herbert Parentes Fortes Neto <hpfn@debian.org>
2018-01-05 17:49:20 -0200

    formating

--- charcoallog/core/views.py
+++ charcoallog/core/views.py
@@ -20,44 +20,39 @@ def home(request):
 @login_required
 @require_POST
 def ajax_post(request):
+    data = dict()
     form = EditExtractForm(request.POST)
     if form.is_valid() and request.is_ajax():
         what_to_do = form.cleaned_data.get('update_rm')
-        del form.cleaned_data['update_rm']
         id_for_update = form.cleaned_data.get('pk')
+        payment = form.cleaned_data.get('payment')
+
+        del form.cleaned_data['update_rm']
         del form.cleaned_data['pk']
 
         form.cleaned_data['user_name'] = request.user
 
-        payment = form.cleaned_data.get('payment')
-        payment_confirm = Extract.objects.user_logged(request.user).filter(
-            payment=payment).first()
+        query_user = Extract.objects.user_logged(request.user)
+
+        payment_confirm = query_user.filter(payment=payment).first()
 
         if not payment_confirm:
-            # print('not payment_confirm')
-            # send this to JS alert
             data = {'no_account': True,
                     'message': 'You can not set a new account name from here'}
-            return JsonResponse(data)
+        else:
+            if what_to_do == 'remove':
+                query_user.filter(**form.cleaned_data).delete()
+            elif what_to_do == 'update':
+                obj = query_user.get(id=id_for_update)  # , user_name=self.request_user)
+                obj.date = form.cleaned_data['date']
+                obj.money = form.cleaned_data['money']
+                obj.description = form.cleaned_data['description']
+                obj.category = form.cleaned_data['category']
+                obj.payment = form.cleaned_data['payment']
+                obj.save(update_fields=['date', 'money', 'description', 'category', 'payment'])
 
-        if what_to_do == 'remove':
-            Extract.objects.user_logged(request.user).filter(**form.cleaned_data).delete()
-        elif what_to_do == 'update':
-            obj = Extract.objects.user_logged(request.user).get(id=id_for_update)  # , user_name=self.request_user)
-            obj.date = form.cleaned_data['date']
-            obj.money = form.cleaned_data['money']
-            obj.description = form.cleaned_data['description']
-            obj.category = form.cleaned_data['category']
-            obj.payment = form.cleaned_data['payment']
-            obj.save(update_fields=['date', 'money', 'description', 'category', 'payment'])
-
-    line1 = Line1(Extract.objects.user_logged(request.user))
-    # Get the TOTAL for line3.html. Save extra lines in ajax_core.js
-    # How to get the date period made by the user
-    # total_line3 = MethodGet("POST", request.POST, Extract.objects.user_logged(request.user))
-
-    data = {'accounts': line1.account_names(),
-            'whats_left': line1.whats_left()}
-    # 'total_line3': total_line3.query_default_total['money__sum']}
+            line1 = Line1(query_user)
+            data = {'accounts': line1.account_names(),
+                    'whats_left': line1.whats_left()}
 
     return JsonResponse(data)
