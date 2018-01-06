e3a5e3895258c4ef2b0ffab0ed4cb74bd052de7e
rg3915 <rg3915@yahoo.com.br>
2018-01-06 13:25:34 -0200

    update admin

--- charcoallog/core/admin.py
+++ charcoallog/core/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from .models import Extract
 
-admin.site.register(Extract)
+
+@admin.register(Extract)
+class ExtractAdmin(admin.ModelAdmin):
+    list_display = ('user_name', 'description', 'money', 'date')
