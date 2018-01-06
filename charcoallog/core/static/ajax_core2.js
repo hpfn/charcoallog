3eae8f0fc4704f7019ae6a1f2e6a911b29b2ed50
rg3915 <rg3915@yahoo.com.br>
2018-01-06 13:26:02 -0200

    If is description the width is large

--- charcoallog/core/static/ajax_core2.js
+++ charcoallog/core/static/ajax_core2.js
@@ -10,7 +10,13 @@ $(function() {
       var item_v = $(this).text()
       if (i < totalLen - 1) {
         $(this).empty()
-        $(this).append('<input type="text" style="width:7em" value="' + item_v + '" />')
+        // If is description the width is large
+        if (i == 2) {
+          console.log(i, item_v);
+          $(this).append('<input type="text" style="width: 20em;" value="' + item_v + '" />')
+        } else {
+          $(this).append('<input type="text" style="width: 6em;" value="' + item_v + '" />')
+        }
       }
     });
   });
