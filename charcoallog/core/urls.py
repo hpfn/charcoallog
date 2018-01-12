96ea77d3cd33b31f1c63c3f4605c2f6d4e8f7f51
Herbert Parentes Fortes Neto <hpfn@debian.org>
2018-01-12 13:05:01 -0200

    Revert "Update money"
    This reverts commit 5ba0b64038ac3ee2562df957a4f186f77e192daa.

--- charcoallog/core/urls.py
+++ charcoallog/core/urls.py
@@ -4,7 +4,7 @@ from charcoallog.core import views as v
 urlpatterns = [
     url(r'^$', v.home, name='home'),
     url(r'^v2/$', v.home2, name='home2'),
-    url(r'^v2/(?P<pk>\d+)/edit/$', v.money_edit, name='money_edit'),
+    # url(r'^v2/(?P<pk>\d+)/edit/$', v.money_edit, name='money_edit'),
     url(r'^v2/(?P<pk>\d+)/delete/$', v.money_delete, name='money_delete'),
     url(r'^ajax_post/', v.ajax_post, name='ajax_post')
 ]
