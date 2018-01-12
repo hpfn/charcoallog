326a81910bad15c34b9d210893302e16c5b59082
Herbert Parentes Fortes Neto <hpfn@debian.org>
2018-01-12 13:07:27 -0200

    Revert "update create_data"
    This reverts commit 11a7f48eb5ec76d3b50482043ed9914a16c8e5d5.

--- create_data.py
+++ create_data.py
@@ -6,15 +6,9 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "charcoallog.settings")
 django.setup()
 
 from django.contrib.auth.models import User
-from django.utils import timezone
 from django.utils.text import slugify
 from mixer.backend.django import mixer
 from charcoallog.core.models import Extract
 
 
-mixer.cycle(5).blend(
-    Extract,
-    user_name='admin',
-    date=timezone.now(),
-    payment=mixer.RANDOM('débito', 'crédito')
-)
+mixer.cycle(5).blend(Extract)
