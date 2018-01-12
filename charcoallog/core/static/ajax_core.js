7924cf93ebfb8650cc6e978c9b22b2fca9ba07fc
Herbert Parentes Fortes Neto <hpfn@debian.org>
2018-01-12 10:01:24 -0200

    rm 'class' instead of 'id' if not_present

--- charcoallog/core/static/ajax_core.js
+++ charcoallog/core/static/ajax_core.js
@@ -73,7 +73,7 @@ $(function() {
                     console.log(not_present);
                     if ( not_present ) {
                         if ( old_account) {
-                            $("[id='"+old_account+"']").text('0');
+                            $("[class='"+old_account+"']").text('0');
                         } else {
                             $("[class='"+data_v[7].value+"").remove();
                         }
