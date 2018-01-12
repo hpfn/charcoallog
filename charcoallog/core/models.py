e673cb8848e6c3cb1584ad3854759e476c145d6c
Herbert Parentes Fortes Neto <hpfn@debian.org>
2018-01-12 13:30:10 -0200

    what to do to prevent duplication after a reload?

--- charcoallog/core/models.py
+++ charcoallog/core/models.py
@@ -30,6 +30,8 @@ class Extract(models.Model):
     objects = models.Manager.from_queryset(ExtractStatementQuerySet)()
 
     def save(self, *args, **kwargs):
+        # this seems useless. a reload will create
+        # a new entry
         if Extract.objects.filter(
                 id=self.id, user_name=self.user_name,
                 date=self.date, money=self.money,
