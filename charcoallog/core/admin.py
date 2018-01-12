a25ccc200c369143fc4a729a86fe28eb992b8138
Herbert Parentes Fortes Neto <hpfn@debian.org>
2018-01-12 13:08:08 -0200

    Revert "update admin"
    This reverts commit e3a5e3895258c4ef2b0ffab0ed4cb74bd052de7e.

--- charcoallog/core/admin.py
+++ charcoallog/core/admin.py
@@ -1,7 +1,4 @@
 from django.contrib import admin
 from .models import Extract
 
-
-@admin.register(Extract)
-class ExtractAdmin(admin.ModelAdmin):
-    list_display = ('user_name', 'description', 'money', 'date')
+admin.site.register(Extract)
