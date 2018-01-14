290c9858f42afee5dd9bce736f0eecf3ccac0fa6
Herbert Parentes Fortes Neto <hpfn@debian.org>
2018-01-14 10:26:42 -0200

    start to split ajax_post view

--- charcoallog/core/views.py
+++ charcoallog/core/views.py
@@ -30,25 +30,47 @@ def ajax_post(request):
             data = {'no_account': True,
                     'message': 'You can not set a new account name from here'}
         else:
-            what_to_do = form.cleaned_data.get('update_rm')
-            id_for_update = form.cleaned_data.get('pk')
-            del form.cleaned_data['update_rm']
-            del form.cleaned_data['pk']
-            form.cleaned_data['user_name'] = request.user
+            what_to_do, id_for_update, form = prepare_action(form, request.user)
+            # what_to_do = form.cleaned_data.get('update_rm')
+            # id_for_update = form.cleaned_data.get('pk')
+            # del form.cleaned_data['update_rm']
+            # del form.cleaned_data['pk']
+            # form.cleaned_data['user_name'] = request.user
 
             if what_to_do == 'remove':
                 query_user.filter(**form.cleaned_data).delete()
             elif what_to_do == 'update':
-                obj = query_user.get(id=id_for_update)  # , user_name=self.request_user)
-                obj.date = form.cleaned_data['date']
-                obj.money = form.cleaned_data['money']
-                obj.description = form.cleaned_data['description']
-                obj.category = form.cleaned_data['category']
-                obj.payment = form.cleaned_data['payment']
-                obj.save(update_fields=['date', 'money', 'description', 'category', 'payment'])
+                update_data(query_user, id_for_update, form)
+                # obj = query_user.get(id=id_for_update)  # , user_name=self.request_user)
+                # obj.date = form.cleaned_data['date']
+                # obj.money = form.cleaned_data['money']
+                # obj.description = form.cleaned_data['description']
+                # obj.category = form.cleaned_data['category']
+                # obj.payment = form.cleaned_data['payment']
+                # obj.save(update_fields=['date', 'money', 'description', 'category', 'payment'])
 
             line1 = Line1(query_user)
             data = {'accounts': line1.account_names(),
                     'whats_left': line1.whats_left()}
 
     return JsonResponse(data)
+
+
+def update_data(query_user, id_for_update, form):
+    obj = query_user.get(id=id_for_update)  # , user_name=self.request_user)
+    obj.date = form.cleaned_data['date']
+    obj.money = form.cleaned_data['money']
+    obj.description = form.cleaned_data['description']
+    obj.category = form.cleaned_data['category']
+    obj.payment = form.cleaned_data['payment']
+    obj.save(update_fields=['date', 'money', 'description', 'category', 'payment'])
+
+
+def prepare_action(form, request_user):
+    what_to_do = form.cleaned_data.get('update_rm')
+    id_for_update = form.cleaned_data.get('pk')
+    del form.cleaned_data['update_rm']
+    del form.cleaned_data['pk']
+    form.cleaned_data['user_name'] = request_user
+
+    return what_to_do, id_for_update, form
