a3bc797d66bce22e4097e02a4641e9bfe6cae882
Herbert Parentes Fortes Neto <hpfn@debian.org>
2018-01-21 10:22:16 -0200

    Uncomment ajax_post call

--- charcoallog/bank/urls.py
+++ charcoallog/bank/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
-from charcoallog.bank.views import home #, ajax_post
+from charcoallog.bank.views import home, ajax_post
 
 urlpatterns = [
     url(r'^$', home, name='home'),
-#    url(r'^ajax_post/', ajax_post, name='ajax_post')
+    url(r'^ajax_post/', ajax_post, name='ajax_post')
 ]
