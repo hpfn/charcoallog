7add52967f6b967deb3ab79fc8d0ee950925092e
Herbert Parentes Fortes Neto <hpfn@debian.org>
2018-01-23 14:43:19 -0200

    first investment model

--- charcoallog/investment/models.py
+++ charcoallog/investment/models.py
@@ -1,3 +1,26 @@
-from django.db import models
+# from django.db import models
+#
+#
+# class BrokerFirm(models.Model):
+#     brokerage = models.CharField(max_length=30)
+#
+#
+# class KindOfInvest(models.Model):
+#     # Nome Acao, Tesouro, CDB, FII
+#     kind = models.CharField(max_length=20)
+#     # Qual acao, tesouro, banco(CDB), cod FII
+#     which_target = models.CharField(max_length=20)
+#     # PN|ON, NTNB|SELIC|LTF, carencia CDB, QUANT FII
+#     segment = models.CharField(max_length=10)
+#     # VALOR cada acao, taxa Tesouro, taxa CDB, valor de compra|venda FII
+#     tx_or_price = models.DecimalField(max_length=8, decimal_places=2)
+#     quant = models.DecimalField(max_length=8)
+#
+#
+# class InvestmentDetails(models.Model):
+#     date = models.DateField()
+#     # money invested
+#     money = models.DecimalField(max_digits=8, decimal_places=2)
+#     kind_of_investment = models.ForeignKey(KindOfInvest)
+#     brokerage_name = models.ManyToManyField(BrokerFirm)
 
-# Create your models here.
