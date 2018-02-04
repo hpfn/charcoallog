83c81e886f05a73a176642ce696b321f74c479fb
Herbert Parentes Fortes Neto <hpfn@debian.org>
2018-02-04 10:56:21 -0200

    Simplify hdr in ibov_info.

--- charcoallog/core/scrap_line3_service.py
+++ charcoallog/core/scrap_line3_service.py
@@ -25,12 +25,14 @@ class Scrap:
 
     def ibov_info(self):
         try:
-            hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
-             'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-             'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
-             'Accept-Encoding': 'none',
-             'Accept-Language': 'en-US,en;q=0.8',
-             'Connection': 'keep-alive'}
+            hdr = {
+                'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+             # 'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+             # 'Accept-Encoding': 'none',
+             # 'Accept-Language': 'en-US,en;q=0.8',
+             # 'Connection': 'keep-alive'
+            }
             req = request.Request(self.ibov_address, headers=hdr)
             html_doc = request.urlopen(req)
             soup = BeautifulSoup(html_doc, 'html.parser')
