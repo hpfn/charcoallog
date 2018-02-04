ede310caa9c95851800125ae4ed31456e47e60b1
Herbert Parentes Fortes Neto <hpfn@debian.org>
2018-02-04 12:42:24 -0200

    One method only. subTest

--- charcoallog/core/tests/test_scraping_bs4_line3.py
+++ charcoallog/core/tests/test_scraping_bs4_line3.py
@@ -4,15 +4,25 @@ from charcoallog.core.scrap_line3_service import Scrap
 
 
 class ScrapTest(TestCase):
-    def setUp(self):
-        self.context = Scrap()
+    # def setUp(self):
+    #    self.context = Scrap()
+    def test_info_type(self):
+        context = Scrap()
+        contents = [
+            context.selic_info(),
+            context.ibov_info(),
+            context.ipca_info(),
+        ]
+        for content in contents:
+            with self.subTest():
+                self.assertIsInstance(content, type(dict()))
 
-    def test_selic_info_type(self):
-        self.assertIsInstance(self.context.selic_info(), type(dict()))
-
-    def test_ibov_info_type(self):
-        self.assertIsInstance(self.context.ibov_info(), type(dict()))
-
-    def test_ipca_info_type(self):
-        self.assertIsInstance(self.context.ipca_info(), type(dict()))
-
+    # def test_selic_info_type(self):
+    #     self.assertIsInstance(self.context.selic_info(), type(dict()))
+    #
+    # def test_ibov_info_type(self):
+    #     self.assertIsInstance(self.context.ibov_info(), type(dict()))
+    #
+    # def test_ipca_info_type(self):
+    #     self.assertIsInstance(self.context.ipca_info(), type(dict()))
+    #
