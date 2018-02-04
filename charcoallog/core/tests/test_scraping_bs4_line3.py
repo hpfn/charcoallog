5829d3d6f5ad99e6bfbc6c73263a297bc6b71968
Herbert Parentes Fortes Neto <hpfn@debian.org>
2018-02-04 12:16:35 -0200

    Test with setUp method.

--- charcoallog/core/tests/test_scraping_bs4_line3.py
+++ charcoallog/core/tests/test_scraping_bs4_line3.py
@@ -1,13 +1,18 @@
-from bs4 import BeautifulSoup
+# from bs4 import BeautifulSoup
 from django.test import TestCase
 from charcoallog.core.scrap_line3_service import Scrap
 
 
 class ScrapTest(TestCase):
+    def setUp(self):
+        self.context = Scrap()
+
     def test_selic_info_type(self):
-        context = Scrap()
-        self.assertIsInstance(context.selic_info(), type(list()))
+        self.assertIsInstance(self.context.selic_info(), type(dict()))
 
     def test_ibov_info_type(self):
-        context = Scrap()
-        self.assertIsInstance(context.ibov_info(), type(dict()))
+        self.assertIsInstance(self.context.ibov_info(), type(dict()))
+
+    def test_ipca_info_type(self):
+        self.assertIsInstance(self.context.ipca_info(), type(dict()))
+
