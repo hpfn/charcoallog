4750b6f8566114a5bc12787711621669b9cc5506
Herbert Parentes Fortes Neto <hpfn@debian.org>
2018-04-10 16:35:59 -0300

    Investments Line1 seems complete - needs better code.

--- charcoallog/investments/brief_investment_service.py
+++ charcoallog/investments/brief_investment_service.py
@@ -7,6 +7,7 @@ class BriefInvestment:
     def __init__(self):
         self.query_user = Investment.objects
         self.account_values = None
+        self.investment_amount = None
 
     def brokerage_names(self):
         names_iterator = set(self.query_user.values_list('brokerage'))
@@ -22,3 +23,18 @@ class BriefInvestment:
 
     def total_amount(self):
         return sum([resto['money__sum'] for resto in self.account_values])
+
+    def investment_types(self):
+        names_iterator = set(self.query_user.values_list('kind'))
+
+        account = {
+            conta[0]: self.query_user.filter(kind=conta[0]).total()
+            for conta in names_iterator
+        }
+
+        self.investment_amount = account.values()
+
+        return OrderedDict(sorted(account.items()))
+
+    def investment_total_amount(self):
+        return sum([resto['money__sum'] for resto in self.investment_amount])
