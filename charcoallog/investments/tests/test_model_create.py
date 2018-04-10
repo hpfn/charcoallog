14f4ee4191c50c6e309b24200131c79d22b6a61d
Herbert Parentes Fortes Neto <hpfn@debian.org>
2018-04-10 10:11:56 -0300

    Test if exists and can be updated.

--- charcoallog/investments/tests/test_model_create.py
+++ charcoallog/investments/tests/test_model_create.py
@@ -3,8 +3,8 @@ from charcoallog.investments.models import Investment, InvestmentDetails
 
 
 class InvestmentModelTest(TestCase):
-    def test_create_investment(self):
-        """ WTTD M3A03 """
+    """ M3A03 - WTTD """
+    def setUp(self):
         Investment.objects.create(
             date='2018-03-27',
             tx_op=00.00,
@@ -13,15 +13,21 @@ class InvestmentModelTest(TestCase):
             which_target='Tesouro Direto',
             brokerage='Ativa'
         )
+
+    def test_investments_exists(self):
+        """ Test if investment is created"""
         self.assertTrue(Investment.objects.exists())
 
-    def test_create_investment_details(self):
-        InvestmentDetails.objects.create(
-            date='2018-03-27',
-            money=94.42,
-            kind='Títulos Públicos',
-            which_target='Tesouro Direto',
-            segment='Selic 2023',
-            tx_or_price=0.01,
-            quant=1.00,
-        )
+    def test_investment_details_exists(self):
+        """ Test if details is created """
+        self.assertTrue(InvestmentDetails.objects.exists())
+
+    def test_update_investment_details(self):
+        """ Test if created details object can be updated """
+        obj = InvestmentDetails.objects.get()
+        obj.segment = 'Selic 2023'
+        obj.tx_or_price = 0.01
+        obj.quant = 1.00
+        obj.save(update_fields=['segment', 'tx_or_price', 'quant'])
+
+        self.assertTrue(InvestmentDetails.objects.filter(segment='Selic 2023').exists())
