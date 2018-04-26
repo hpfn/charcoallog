b8a907fd4dece2c70ee0e48758f13fb4f85354b2
Herbert Parentes Fortes Neto <hpfn@debian.org>
2018-04-26 10:03:34 -0300

    detail view shows a '.get(pk=pk)' in .html

--- charcoallog/investments/tests/test_investment_detail.py
+++ charcoallog/investments/tests/test_investment_detail.py
@@ -12,7 +12,7 @@ class InvestmentDetailTest(TestCase):
 
         self.login_in = self.client.login(username='teste', password='1qa2ws3ed')
 
-        self.obj = InvestmentDetails.objects.create(
+        self.data = dict(
             user_name='teste',
             date='2018-03-27',
             money=94.42,
@@ -22,6 +22,8 @@ class InvestmentDetailTest(TestCase):
             tx_or_price=0.01,
             quant=1.00
         )
+
+        self.obj = InvestmentDetails.objects.create(**self.data)
         self.resp = self.client.get(r('investments:detail', self.obj.pk))
 
     def test_login(self):
@@ -33,6 +35,15 @@ class InvestmentDetailTest(TestCase):
          self.assertEqual(200, self.resp.status_code)
 
     def test_template_used(self):
-
         self.assertTemplateUsed(self.resp, 'investments/detail.html')
+
+    def test_context(self):
+        data = self.resp.context['d']
+        self.assertIsInstance(data, InvestmentDetails)
+
+    def test_context(self):
+        data = self.resp.context['d']
+        self.assertEqual(data.kind, self.data['kind'])
+        self.assertEqual(data.which_target, self.data['which_target'])
+
 
--- charcoallog/investments/views.py
+++ charcoallog/investments/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from charcoallog.investments.forms import InvestmentForm
+from charcoallog.investments.models import InvestmentDetails
 from charcoallog.investments.service import ShowData
 
 
@@ -14,4 +15,7 @@ def home(request):
 
 @login_required
 def detail(request, pk):
-    return render(request, 'investments/detail.html', {})
+    context = {
+        'd': InvestmentDetails.objects.get(pk=pk),
+    }
+    return render(request, 'investments/detail.html', context)
