f9dd7b16c2633a3e26d452f292ff7f1b27d5401a
Herbert Parentes Fortes Neto <hpfn@debian.org>
2018-05-13 11:31:16 -0300

    PEP8

--- charcoallog/bank/models.py
+++ charcoallog/bank/models.py
@@ -29,19 +29,5 @@ class Extract(models.Model):
 
     objects = models.Manager.from_queryset(ExtractStatementQuerySet)()
 
-    #def save(self, *args, **kwargs):
-    #    # this seems useless. a reload will create
-    #    # a new entry
-    #    if Extract.objects.filter(
-    #            id=self.id, user_name=self.user_name,
-    #            date=self.date, money=self.money,
-    #            description=self.description, category=self.category,
-    #            payment=self.payment).exists():
-    #        print("problemas")
-    #        pass
-    #        #   return
-    #    else:
-    #        super(Extract, self).save(*args, **kwargs)
-
     class Meta:
         ordering = ['-date']
