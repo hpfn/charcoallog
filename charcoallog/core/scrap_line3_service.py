31319cd5ccc7285cb029836959f5ee04449fa40b
Herbert Parentes Fortes Neto <hpfn@debian.org>
2018-06-02 13:48:19 -0300

    DRY - tasks and scrap_line3_service - core app
    close #29

--- charcoallog/core/scrap_line3_service.py
+++ charcoallog/core/scrap_line3_service.py
@@ -21,23 +21,7 @@ class Scrap:
 
             return selic['selic']
 
-        data = date.today()
-        this_year = data.strftime("%Y")
-        last_year = str(int(this_year) - 1)
-
-        html_doc = request.urlopen(self.selic_address)
-        soup = BeautifulSoup(html_doc, 'html.parser')
-        tabela = soup.find_all("td", class_="centralizado")
-
-        #        tabela_dict = {i.string: tabela[x+1].string
-        #                       for x, i in enumerate(tabela)
-        #                       if last_year in i.string or this_year in i.string}
-
-        tabela_dict = ([i.string, tabela[x + 1].string]
-                       for x, i in enumerate(tabela)
-                       if last_year in i.string or this_year in i.string)
-
-        return tabela_dict
+        return self.selic_webscrapping()
 
     def ibov_info(self):
         if os.path.isfile('./charcoallog/core/ibov.json'):
@@ -46,16 +30,43 @@ class Scrap:
 
             return ibov['ibov']
 
+        return self.ibov_webscrapping()
+
+    def ipca_info(self):
+        if os.path.isfile('./charcoallog/core/ipca.json'):
+            with open('./charcoallog/core/ipca.json', 'r') as ipca:
+                ipca = json.load(ipca)
+
+            return ipca['ipca']
+
+        return self.ipca_webscrapping()
+
+    def selic_webscrapping(self):
+        # data = date.today()
+        this_year = date.today().strftime("%Y")
+        last_year = str(int(this_year) - 1)
+
+        html_doc = request.urlopen(self.selic_address)
+        soup = BeautifulSoup(html_doc, 'html.parser')
+        tabela = soup.find_all("td", class_="centralizado")
+
+        tabela_dict = [[i.string, tabela[x + 1].string]
+                       for x, i in enumerate(tabela)
+                       if str(last_year) in i.string or str(this_year) in i.string]
+
+        return tabela_dict
+
+    def ibov_webscrapping(self):
         try:
             hdr = {
-                 'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) '
-                               'AppleWebKit/537.11 (KHTML, like Gecko) '
-                               'Chrome/23.0.1271.64 Safari/537.11',
-                 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-                 # 'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
-                 # 'Accept-Encoding': 'none',
-                 # 'Accept-Language': 'en-US,en;q=0.8',
-                 # 'Connection': 'keep-alive'
+                'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) '
+                              'AppleWebKit/537.11 (KHTML, like Gecko) '
+                              'Chrome/23.0.1271.64 Safari/537.11',
+                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+                # 'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+                # 'Accept-Encoding': 'none',
+                # 'Accept-Language': 'en-US,en;q=0.8',
+                # 'Connection': 'keep-alive'
             }
 
             req = request.Request(self.ibov_address, headers=hdr)
@@ -71,23 +82,14 @@ class Scrap:
             column3 = soup.find_all("td", class_="Numeric Column10 ColumnLast")
             column3.insert(0, tabela_hdr[2])
 
-            # head_dict = {col1.string: [col2.string, col3.string]
-            #             for col1, col2, col3 in zip(column1, column2, column3)}
-
-            head_dict = ([col1.string, [col2.string, col3.string]]
-                         for col1, col2, col3 in zip(column1, column2, column3))
+            head_dict = [[col1.string, [col2.string, col3.string]]
+                         for col1, col2, col3 in zip(column1, column2, column3)]
 
             return head_dict
         except HTTPError:
             return ['http error']
 
-    def ipca_info(self):
-        if os.path.isfile('./charcoallog/core/ipca.json'):
-            with open('./charcoallog/core/ipca.json', 'r') as ipca:
-                ipca = json.load(ipca)
-
-            return ipca['ipca']
-
+    def ipca_webscrapping(self):
         html_doc = request.urlopen(self.ipca_address)
         soup = BeautifulSoup(html_doc, 'html.parser')
 
@@ -104,11 +106,7 @@ class Scrap:
 
         get_tx = re.findall(taxas, str(tabela_bd))
         get_tx = [i[3] for i in get_tx]
-        # get_tx = [i for i in get_tx if float(i) > 0.85]
 
-        # tx_ano_dict = {ano: tx for ano, tx in zip(get_ano[:10], get_tx[:10])}
-        tx_ano_dict = ([ano, tx] for ano, tx in zip(get_ano[:10], get_tx[:10]))
-        # print(get_ano[:10])
-        # print(get_tx[:10])
+        tx_ano_dict = [[ano, tx] for ano, tx in zip(get_ano[:10], get_tx[:10])]
 
         return tx_ano_dict
