67033712c453934473d6456bedda49cdcb09388c
Herbert Parentes Fortes Neto <hpfn@debian.org>
2018-06-02 13:48:19 -0300

    DRY - tasks and scrap_line3_service - core app
    close #29

--- charcoallog/core/tasks.py
+++ charcoallog/core/tasks.py
@@ -1,20 +1,10 @@
-# from celery import Celery
 import json
-import re
-from datetime import date
-from urllib import request
 
-from bs4 import BeautifulSoup
 from celery.schedules import crontab
 from celery.utils.log import get_task_logger
-# from charcoallog.celery import app as celery_app
-# RealPython.com
-# from celery.decorators import periodic_task
 from celery.task import periodic_task
 
-# app = Celery('charcoallog')
-# @app.on_after_configure
-
+from charcoallog.core.scrap_line3_service import Scrap
 
 logger = get_task_logger(__name__)
 
@@ -23,38 +13,8 @@ logger = get_task_logger(__name__)
 def ibov():
     logger.info('inicio de core/ibov.json')
 
-    hdr = {
-        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) '
-                      'AppleWebKit/537.11 (KHTML, like Gecko) '
-                      'Chrome/23.0.1271.64 Safari/537.11',
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-        # 'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
-        # 'Accept-Encoding': 'none',
-        # 'Accept-Language': 'en-US,en;q=0.8',
-        # 'Connection': 'keep-alive'
-    }
-
-    ibov_address = 'https://br.advfn.com/bolsa-de-valores/bovespa/ibovespa-IBOV/historico'
-    req = request.Request(ibov_address, headers=hdr)
-    html_doc = request.urlopen(req)
-    soup = BeautifulSoup(html_doc, 'html.parser')
-    tabela_hdr = soup.find_all(
-        "th",
-        class_=re.compile("Column(1|2|10).?(ColumnLast)? (String|Numeric)"))
-    column1 = soup.find_all("td", class_="String Column1")
-    column1.insert(0, tabela_hdr[0])
-    column2 = soup.find_all("td", class_="Numeric Column2")
-    column2.insert(0, tabela_hdr[1])
-    column3 = soup.find_all("td", class_="Numeric Column10 ColumnLast")
-    column3.insert(0, tabela_hdr[2])
-
-    # head_dict = {col1.string: [col2.string, col3.string]
-    #             for col1, col2, col3 in zip(column1, column2, column3)}
-
-    head_dict = [[col1.string, [col2.string, col3.string]]
-                 for col1, col2, col3 in zip(column1, column2, column3)]
-
-    json_data = {"ibov": head_dict}
+    ibov_stuff = Scrap()
+    json_data = {"ibov": ibov_stuff.ibov_webscrapping()}
 
     with open('./charcoallog/core/ibov.json', 'w') as ibov_info:
         json.dump(json_data, ibov_info)
@@ -66,21 +26,8 @@ def ibov():
 def selic():
     logger.info('inicio de core/selic.json')
 
-    data = date.today()
-    this_year = data.strftime("%Y")
-    last_year = str(int(this_year) - 1)
-
-    selic_address = 'https://www.bcb.gov.br/Pec/Copom/Port/taxaSelic.asp'
-
-    html_doc = request.urlopen(selic_address)
-    soup = BeautifulSoup(html_doc, 'html.parser')
-    tabela = soup.find_all("td", class_="centralizado")
-
-    tabela_dict = [[i.string, tabela[x + 1].string]
-                   for x, i in enumerate(tabela)
-                   if last_year in i.string or this_year in i.string]
-
-    json_data = {"selic": tabela_dict}
+    selic_stuff = Scrap()
+    json_data = {"selic": selic_stuff.selic_webscrapping()}
 
     with open('./charcoallog/core/selic.json', 'w') as selic_info:
         json.dump(json_data, selic_info)
@@ -92,27 +39,8 @@ def selic():
 def ipca():
     logger.info('inicio de core/ipca.json')
 
-    ipca_address = 'http://www.indiceseindicadores.com.br/ipca/'
-
-    html_doc = request.urlopen(ipca_address)
-    soup = BeautifulSoup(html_doc, 'html.parser')
-
-    tabela_bd = soup.find("tbody")
-
-    ano = re.compile(r'<strong>\b(?P<ano>[0-9]{4})\b</strong>')
-    get_ano = re.findall(ano, str(tabela_bd))
-
-    # taxas = re.compile(r'<(strong|b)>\b(?P<indice>[0-9]{,2},[0-9]{2})\b</(strong|b)>')
-    taxas = re.compile(
-        r'<td style="text-align: right; width: [0-9.]{3,}px; height: [0-9.]{2,}px;">'
-        r'(<span style="font-size: 10pt;">)?(<(strong|b)>)?\b(?P<indice>[0-9]{,2},[0-9]{2})\b'
-        r'(</(strong|b)>)?(</span>)?</td>')
-
-    get_tx = re.findall(taxas, str(tabela_bd))
-    get_tx = [i[3] for i in get_tx]
-    tx_ano_dict = [[ano, tx] for ano, tx in zip(get_ano[:10], get_tx[:10])]
-
-    json_data = {"ipca": tx_ano_dict}
+    ipca_stuff = Scrap()
+    json_data = {"ipca": ipca_stuff.ipca_webscrapping()}
 
     with open('./charcoallog/core/ipca.json', 'w') as ipca_info:
         json.dump(json_data, ipca_info)
--- charcoallog/core/tests/test_scraping_bs4_line3.py
+++ charcoallog/core/tests/test_scraping_bs4_line3.py
@@ -1,48 +1,100 @@
 # from bs4 import BeautifulSoup
+import os
+
 from django.test import TestCase
+from unittest.mock import patch
 
-
-class FakeScrap:
-    """ Evitando testes. Aprender mock """
-
-    def __init__(self):
-        self.selic_address = 'https://www.bcb.gov.br/Pec/Copom/Port/taxaSelic.asp'
-        self.ibov_address = 'https://br.advfn.com/bolsa-de-valores/bovespa/ibovespa-IBOV/historico'
-        self.ipca_address = 'http://www.indiceseindicadores.com.br/ipca/'
-
-    def selic_info(self):
-        return [['22/03/2018 -', '6.50'], ['08/02/2018 - 21/03/2018', '6,75'],
-                ['07/12/2017 - 07/02/2018', '7,00'], ['26/10/2017 - 06/12/2017', '7,50']]
-
-    def ibov_info(self):
-        return ['periodo', ['abertura', 'percentual']]
-
-    def ipca_info(self):
-        get_ano = ['2018', '2017', '2016', '2015']
-        get_tx = ['0.61', '2.95', '6.29', '10.67']
-        return [[ano, tx] for ano, tx in zip(get_ano, get_tx[:10])]
-
+from charcoallog.core.scrap_line3_service import Scrap
 
 class ScrapTest(TestCase):
-    # def setUp(self):
-    #    self.context = Scrap()
-    def test_info_type(self):
-        context = FakeScrap()
-        contents = [
-            context.selic_info(),
-            context.ibov_info(),
-            context.ipca_info(),
-        ]
-        for content in contents:
-            with self.subTest():
-                self.assertIsInstance(content, type(list()))
+    # @patch('os.path.isfile')
+    # @patch('charcoallog.core.scrap_line3_service.request')
+    # def test_info_type(self, request, check_file):
+    #     check_file.return_value = True
+    #     context = Scrap()
+    #     contents = [
+    #         context.selic_info(),
+    #         context.ibov_info(),
+    #         context.ipca_info(),
+    #     ]
+    #     for content in contents:
+    #         with self.subTest():
+    #             self.assertIsInstance(content, type(list()))
 
-    # def test_selic_info_type(self):
-    #     self.assertIsInstance(self.context.selic_info(), type(dict()))
-    #
-    # def test_ibov_info_type(self):
-    #     self.assertIsInstance(self.context.ibov_info(), type(dict()))
-    #
-    # def test_ipca_info_type(self):
-    #     self.assertIsInstance(self.context.ipca_info(), type(dict()))
-    #
+    @patch('charcoallog.core.scrap_line3_service.date')
+    def test_selic_webscrapping(self, mocked_date):
+        r = Scrap()
+        BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+        selic_file = os.path.join(BASE_DIR, 'tests/selic.html')
+        r.selic_address = 'file://' + selic_file
+
+        mocked_date.today.strftime.return_value = '2018'
+
+        expected = [
+            ['22/03/2018 - ', '6,50'],
+            ['6,50', '\n'],
+            ['08/02/2018 - 21/03/2018', '6,75'],
+            ['\r\n\t  0,72\r\n\t  ', '\r\n\t  6,65\r\n\r\n\t'],
+            ['07/12/2017 - 07/02/2018', '7,00'],
+            ['7,00', '\r\n\t  1,15\r\n\t  '],
+            ['\r\n\t  6,90\r\n\r\n\t', '26/10/2017 - 06/12/2017'],
+            ['26/10/2017 - 06/12/2017', '7,50'],
+            ['7,50', '\r\n\t  0,80\r\n\t  '],
+            ['\r\n\t  0,80\r\n\t  ', '\r\n\t  7,40\r\n\r\n\t'],
+            ['\r\n\t  7,40\r\n\r\n\t', '08/09/2017 - 25/10/2017'],
+            ['08/09/2017 - 25/10/2017', '8,25'],
+            ['\r\n\t  1,03\r\n\t  ', '\r\n\t  8,15\r\n\r\n\t']
+        ]
+
+        self.assertEqual(r.selic_webscrapping(), expected)
+
+    def test_ibov_scrapping(self):
+        index = Scrap()
+        BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+        ibov_file = os.path.join(BASE_DIR, 'tests/ibov.html')
+        index.ibov_address = 'file://' + ibov_file
+
+        expected = [
+            ['Período', ['Abe', '%']],
+            ['1 Semana', ['80.123,449', '-3,60%']],
+            ['1 Mês', ['83.286,382', '-7,26%']],
+            ['3 Meses', ['84.987,154', '-9,12%']],
+            ['6 Meses', ['71.955,27', '7,34%']],
+            ['1 Ano', ['62.289,195', '24,00%']],
+            ['3 Anos', ['53.517,601', '44,33%']],
+            ['5 Anos', ['52.876,775', '46,08%']]
+        ]
+
+        self.assertEqual(index.ibov_webscrapping(), expected)
+
+
+
+
+
+
+
+
+
+
+    def test_ipca_webscrapping(self):
+        tx = Scrap()
+        BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+        ipca_file = os.path.join(BASE_DIR, 'tests/ipca.html')
+        tx.ipca_address = 'file://' + ipca_file
+
+        expected = [
+            ['2018', '0,92'],
+            ['2017', '2,95'],
+            ['2016', '6,29'],
+            ['2015', '10,67'],
+            ['2014', '6,40'],
+            ['2013', '5,91'],
+            ['2012', '5,83'],
+            ['2011', '6,50'],
+            ['2010', '5,90'],
+            ['2009', '4,31']
+        ]
+
+        self.assertEqual(tx.ipca_webscrapping(), expected)
+
+
