615f1e1c6135bf7764e582450a27679de37ba1c4
Herbert Parentes Fortes Neto <hpfn@debian.org>
2018-08-28 15:49:12 -0300

    DRF - Update and Delete - APIVIEW
    unittest missing

--- charcoallog/investments/views.py
+++ charcoallog/investments/views.py
@@ -1,8 +1,12 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import Http404, render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from charcoallog.investments.forms import BasicDataForm, InvestmentForm
-from charcoallog.investments.models import InvestmentDetails
+from charcoallog.investments.models import Investment, InvestmentDetails
+from charcoallog.investments.serializers import InvestmentSerializer
 from charcoallog.investments.service import ShowData
 
 
@@ -25,3 +29,29 @@ def detail(request, kind):
         'd': qs
     }
     return render(request, 'investments/detail.html', context)
+
+
+class FormDeals(APIView):
+    def get_object(self, pk):
+        try:
+            return Investment.objects.get(pk=pk)
+        except Investment.DoesNotExist:
+            raise Http404
+
+    # def get(self, request, pk, format=None):
+    #     investment = self.get_object(pk)
+    #     serializer = InvestmentSerializer(investment)
+    #     return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        investment = self.get_object(pk)
+        serializer = InvestmentSerializer(investment, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        investment = self.get_object(pk)
+        investment.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
