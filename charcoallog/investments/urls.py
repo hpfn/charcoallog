40d3a2ab036adb2cd8f47bda4f7be031bd9f981e
Herbert Parentes Fortes Neto <hpfn@debian.org>
2018-09-09 16:39:42 -0300

    JS to work with DRF - investments
    close #76

--- charcoallog/investments/urls.py
+++ charcoallog/investments/urls.py
@@ -6,7 +6,7 @@ from charcoallog.investments.views import FormDeals, detail, home
 urlpatterns = [
     url(r'^$', home, name='home'),
     url(r'^([\w ]+)/$', detail, name='detail'),
-    url(r'^update/(?P<pk>[0-9]+)/$', FormDeals.as_view(), name='update'),
+    url(r'^api/(?P<pk>[0-9]+)/$', FormDeals.as_view(), name='api'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
