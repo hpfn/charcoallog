fa24b5086c2428abe5e09d1abcb669727447c1ed
Herbert Parentes Fortes Neto <hpfn@debian.org>
2018-10-16 09:35:14 -0300

    Invest info in core app
    close #156

--- charcoallog/core/service.py
+++ charcoallog/core/service.py
@@ -1,6 +1,8 @@
 from charcoallog.bank.brief_bank_service import BriefBank
 from charcoallog.bank.models import Extract
 from charcoallog.core.scrap_line3_service import Scrap
+from charcoallog.investments.brief_investment_service import BriefInvestment
+from charcoallog.investments.models import NewInvestment, NewInvestmentDetails
 
 
 class BuildHome:
@@ -11,3 +13,7 @@ class BuildHome:
         self.selic_info = tabela.selic_info()
         self.ibov_info = tabela.ibov_info()
         self.ipca_info = tabela.ipca_info()
+
+        self.query_user_invest = NewInvestment.objects.user_logged(request_user)
+        self.query_user_details = NewInvestmentDetails.objects.user_logged(request_user)
+        self.line2 = BriefInvestment(self.query_user_invest, self.query_user_details)
