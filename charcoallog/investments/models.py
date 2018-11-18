0d4e164b289d0b5046a63c4c86cfa530772b3459
Herbert Parentes Fortes Neto <hpfn@debian.org>
2018-11-18 11:41:07 -0200

    Rm old tables, new Meta: ordering, verbose_name
    close #179

--- charcoallog/investments/models.py
+++ charcoallog/investments/models.py
@@ -16,41 +16,41 @@ class InvestmentStatementQuerySet(models.QuerySet):
         return self.values_list('kind')
 
 
-class NewBasicData(models.Model):
-    user_name = models.CharField(max_length=30)
-    date = models.DateField()
-    money = models.DecimalField(max_digits=8, decimal_places=2)
-    # Acao, Titulo Publico, CDB, FII
-    kind = models.CharField(max_length=40, default='---')
+# class NewBasicData(models.Model):
+#    user_name = models.CharField(max_length=30)
+#    date = models.DateField()
+#    money = models.DecimalField(max_digits=8, decimal_places=2)
+#    # Acao, Titulo Publico, CDB, FII
+#    kind = models.CharField(max_length=40, default='---')
+#
+# both a custom Manager and a custom QuerySet
+# https://docs.djangoproject.com/en/1.11/topics/db/managers/#from-queryset
+# objects = models.Manager.from_queryset(InvestmentStatementQuerySet)()
+#
+# to create an instance of Manager with a copy of a custom QuerySet’s
+# https://docs.djangoproject.com/en/1.11/topics/
+# db/managers/#creating-a-manager-with-queryset-methods
+#    objects = InvestmentStatementQuerySet.as_manager()
 
-    # both a custom Manager and a custom QuerySet
-    # https://docs.djangoproject.com/en/1.11/topics/db/managers/#from-queryset
-    # objects = models.Manager.from_queryset(InvestmentStatementQuerySet)()
-    #
-    # to create an instance of Manager with a copy of a custom QuerySet’s
-    # https://docs.djangoproject.com/en/1.11/topics/
-    # db/managers/#creating-a-manager-with-queryset-methods
-    objects = InvestmentStatementQuerySet.as_manager()
-
-    class Meta:
-        abstract = True
-        ordering = ['-date']
-
-
-class OldNewInvestmentDetails(NewBasicData):
-    # Qual acao, titulo publico, banco(CDB), cod FII
-    which_target = models.CharField(max_length=20, default='---')
-    # PN|ON, NTNB|SELIC|LTF, carencia CDB, sobre FII
-    segment = models.CharField(max_length=20, default='---')
-    # VALOR cada acao, taxa Tesouro, taxa CDB, valor de compra|venda FII
-    tx_or_price = models.DecimalField(max_digits=8, decimal_places=2, default=00.00)
-    quant = models.DecimalField(max_digits=8, decimal_places=2, default=00.00)
+#   class Meta:
+#        abstract = True
+#        ordering = ['-date']
 
 
-class OldNewInvestment(NewBasicData):
-    # corretagem
-    tx_op = models.DecimalField(max_digits=4, decimal_places=2)
-    brokerage = models.CharField(max_length=15)
+# class OldNewInvestmentDetails(NewBasicData):
+#     # Qual acao, titulo publico, banco(CDB), cod FII
+#     which_target = models.CharField(max_length=20, default='---')
+#     # PN|ON, NTNB|SELIC|LTF, carencia CDB, sobre FII
+#     segment = models.CharField(max_length=20, default='---')
+#     # VALOR cada acao, taxa Tesouro, taxa CDB, valor de compra|venda FII
+#     tx_or_price = models.DecimalField(max_digits=8, decimal_places=2, default=00.00)
+#     quant = models.DecimalField(max_digits=8, decimal_places=2, default=00.00)
+
+
+# class OldNewInvestment(NewBasicData):
+#     # corretagem
+#     tx_op = models.DecimalField(max_digits=4, decimal_places=2)
+#     brokerage = models.CharField(max_length=15)
 
 
 class NewInvestment(models.Model):
